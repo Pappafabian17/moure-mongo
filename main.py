1d6aa4a7ff932eb164fa5f08fdfb827c5298bc7e
Fabian Pappa <fabian.pappa.ext@errepar.com>
2025-06-02 15:53:37 -0300

    cambios

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from fastapi import FastAPI
-
+from routers import products, users
+from fastapi.staticfiles import StaticFiles
 app = FastAPI()
+#routers
+app.include_router(products.router)
+app.include_router(users.router)
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
-
-
-@app.get("/")
+@app.get("/",tags=["Main"])
 async def root():
   return "!Hola Fastapi!"
 
-@app.get("/url")
+@app.get("/url",tags=["Main"])
 async def url():
   return {"url_curso":"https://mouredev.com"}
